c3e562b831e1c5d9d56d56601f6bf7640957f7ff
SLEAZOIDS <yoshioka@accs-c.co.jp>
2017-10-03 15:24:51 +0900

    view追加

--- app/tmp/main.go
+++ app/tmp/main.go
@@ -36,7 +36,19 @@ func main() {
 				Args: []*revel.MethodArg{ 
 				},
 				RenderArgNames: map[int][]string{ 
-					12: []string{ 
+					13: []string{ 
+						"greeting",
+					},
+				},
+			},
+			&revel.MethodType{
+				Name: "Hello",
+				Args: []*revel.MethodArg{ 
+					&revel.MethodArg{Name: "myName", Type: reflect.TypeOf((*string)(nil)) },
+				},
+				RenderArgNames: map[int][]string{ 
+					17: []string{ 
+						"myName",
 					},
 				},
 			},
